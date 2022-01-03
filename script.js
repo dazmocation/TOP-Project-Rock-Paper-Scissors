e197ffbc9225ac5eb1e8ecb9cd5441ba5fe574c2
daz <dazmocation@gmail.com>
2022-01-03 13:35:29 -0500

    Added game loop, tested.

--- script.js
+++ script.js
@@ -17,39 +17,63 @@ function randomValue(min, max) {
     max = Math.floor(max);
     return Math.floor(Math.random(Date.now()) * (max - min + 1) + min);
 }
-// Plays the game
+// Plays a round of the game
 function playRound(playerSelection) {
     playerSelection = playerSelection.toLowerCase();
-    // scissors beats paper
     computerChoice = computerPlay();
+    // scissors beats paper
     if (playerSelection === "scissors" && computerChoice === "paper" || playerSelection === "paper" && computerChoice === "scissors") {
         if (playerSelection === "scissors") {
             console.log("You win! Scissors beats Paper!")
+            return 1;
         }
         else {
             console.log("You lose! Scissors beats Paper!")
+            return -1;
         }
     }
     // paper beats rock
     else if (playerSelection === "rock" && computerChoice === "paper" || playerSelection === "paper" && computerChoice === "rock") {
         if (playerSelection === "rock") {
             console.log("You lose! Paper beats Rock!")
+            return -1;
         }
         else {
             console.log("You win! Paper beats Rock!")
+            return 1;
         }
     }
     // rock beats scissors
     else if (playerSelection === "rock" && computerChoice === "scissors" || playerSelection === "scissors" && computerChoice === "rock") {
         if (playerSelection === "rock") {
             console.log("You win! Rock beats Scissors!")
+            return 1;
         }
         else {
             console.log("You lose! Rock beats Scissors!")
+            return -1;
         }
     }
     // samesies
     else {
         console.log("You both chose " + playerSelection + ". Try again!");
+        return 0;
+    }
+}
+
+function game() {
+    let score = 0
+    console.log("Let's play a game! Best of 5 wins!")
+    for (let i = 0; i < 5; i++) {
+        score = score + playRound(prompt("What do you want to play? Rock / Paper / Scissors"));
+    }
+    if (score > 0) {
+        console.log("Congratulations! You win!")
+    }
+    else if (score <0) {
+        console.log("Sorry! You lose!")
+    }
+    else {
+        console.log("Oops! You tied! Try again!")
     }
 }
